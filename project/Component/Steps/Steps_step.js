9536502d4d0f7e06381832224c51566feedf83b9
zzp <zzp@qqzi.com>
2018-04-23 19:51:48 +0800

    test

--- project/Component/Steps/Steps_step.js
+++ project/Component/Steps/Steps_step.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react';
 import { View,TouchableNativeFeedback } from 'react-native';
 import { Steps,Toast,Text  } from 'antd-mobile';
-import { data } from '../StaticContent/StaticContent'
 const Step = Steps.Step;
 
 function stepsClick() {
@@ -15,8 +14,8 @@ class StepsA extends Component {
   render() {
     return (
       <View>
-            <Steps current={data.steps.current} size={data.steps.size} direction={data.steps.direction}>
-               {data.steps.step.map((s, i) =><Step key={i} title={s.title} description={s.description}/> )}
+            <Steps current={this.props.data.steps.current} size={this.props.data.steps.size} direction={this.props.data.steps.direction}>
+               {this.props.data.steps.step.map((s, i) =><Step key={i} title={s.title} description={s.description}/> )}
            </Steps>
       </View>
     );
--- project/Test.js
+++ project/Test.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { Text,View,ScrollView  } from 'react-native';
 import { Button,Icon  } from 'antd-mobile';
+import { data } from './Component/StaticContent/StaticContent';
 import StepsA from './Component/Steps/Steps_step';
 class Test extends Component {
   render() {
@@ -9,7 +10,7 @@ class Test extends Component {
       <ScrollView style={{flex:1}}>
       <View style={{flex:1,display:'flex',alignItems:'center',padding:50}}>
 
-             <StepsA />
+             <StepsA data={data}/>
       </View>
       </ScrollView >
     );
