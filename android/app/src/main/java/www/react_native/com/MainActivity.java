64a90ef18fbf6b6b78258abd1f658c59215ebd2c
zzp <zzp@qqzi.com>
2018-05-15 21:36:54 +0800

    ReactActivity2.java

--- android/app/src/main/java/www/react_native/com/MainActivity.java
+++ android/app/src/main/java/www/react_native/com/MainActivity.java
@@ -1,13 +1,33 @@
 package www.react_native.com;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactActivity2 {
+    private View mRootView;
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //初始化rnrootview
+        super.onCreate(savedInstanceState);
+        //获取rnrootview
+        mRootView = getRootView();
+        //模拟loadding图片
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(mRootView);
+            }
+        },3000);
+    }
+
     @Override
     protected String getMainComponentName() {
         return "My_React_Native";
